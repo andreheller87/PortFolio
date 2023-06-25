de1c857f7e69bd32ad5c6017d38c632f04e1a917
andreheller87 <andreheller02@gmail.com>
2023-06-25 18:04:39 -0300

    Upload

--- script.js
+++ script.js
@@ -1,31 +1,30 @@
-function abrirmenu(){
-    let BarraMenuAberto = document.getElementById ('barra-menu');
-    
-    if (BarraMenuAberto.style.width == "0px"){
+function abrirmenu() {
+    let BarraMenuAberto = document.getElementById('barra-menu');
+
+    if (BarraMenuAberto.style.width == "0px") {
         BarraMenuAberto.style.width = "180px";
-    }else{
+    } else {
         BarraMenuAberto.style.width = "0px";
     }
 }
 
 function exibirCurriculo() {
     window.open('curriculo.pdf', '_blank');
-  }
+}
 
-  function exibirHardSkills() {
+function exibirHardSkills() {
     var elementoHardSkills = document.getElementById("HardSkill");
     elementoHardSkills.scrollIntoView({ behavior: 'smooth' });
-  }
+}
 
-  function exibirGitLink() {
+function exibirGitLink() {
     var elementoHardSkills = document.getElementById("gitlink");
     elementoHardSkills.scrollIntoView({ behavior: 'smooth' });
-  }
+}
 
-  function whats() {
+function whats() {
     var url = "https://whatsa.me/5547992847590";
     window.open(url, "_blank");
-  }
-  
+}
 
-  
+
